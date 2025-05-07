63fadcce710044b28ae28ad030eea5d17854febb
Syed Unais <180418315+syedunaisp@users.noreply.github.com>
2025-05-08 00:30:13 +0530

    setup database models

--- finance_tracker/tracker/models.py
+++ finance_tracker/tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    TRANSACTION_TYPE = [('income', 'Income'), ('expense', 'Expense')]
+    type = models.CharField(max_length=7, choices=TRANSACTION_TYPE)
+    date = models.DateField()
+    notes = models.TextField(blank=True, null=True)
