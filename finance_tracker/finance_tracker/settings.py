6bd116b9bcb19ccaa7a61df76af52df9a83bbe86
Adeen16 <180252026+Adeen16@users.noreply.github.com>
2025-05-09 00:53:09 +0530

    tailwind

--- finance_tracker/finance_tracker/settings.py
+++ finance_tracker/finance_tracker/settings.py
@@ -38,8 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tracker',
+    'tracker','tailwind','theme',
+
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ['127.0.0.1']
+
+NPM_BIN_PATH = r"C:\Program Files\NPM\bin"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
